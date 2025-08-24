ffdc26551a74986490e4ebf4eb9f10fd6c04e130
Jason <66013853+ahmola@users.noreply.github.com>
2025-08-24 20:44:45 +0900

    client

--- extension-ui/vite.config.js
+++ extension-ui/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/extension-ui',
+  base: '/ExtensionFilter/',
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://43.200.6.17:8888',
         changeOrigin: true
       }
     }
